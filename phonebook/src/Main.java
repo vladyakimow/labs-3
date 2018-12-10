b7f02ede35fb77580d324e9b2507beb6a63c15c8
Vlad_Yakimow <31766832+vladyakimow@users.noreply.github.com>
2018-12-11 00:05:43 +0300

    Update Main.java

--- phonebook/src/Main.java
+++ phonebook/src/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Телефонная книга");
+        primaryStage.setTitle("PhoneBook");
 
         try {
             FXMLLoader loader = new FXMLLoader(Main.class.getResource("PersonTable.fxml"));
